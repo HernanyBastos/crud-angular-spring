10300785c74f920fc6797840a225170dcb0fc0d4
Hernany Bastos <hernanybastosfig@gmail.com>
2025-02-20 16:25:48 -0300

    Alteração na rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
     redirectTo: 'courses',
   },
   {
-    path: 'items',
+    path: 'courses',
     loadChildren: () => import ('./courses/courses.module').then(m => m.CoursesModule)
   }
 ];
